739ca050c286467f109d22d3fb75b351b10bea68
Darrel Idiagbor <idiagbordarrel@gmail.com>
2021-05-17 21:43:18 +0100

    feat: setup complete

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { hydrate, render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './stylesheet/main.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+if (rootElement?.hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>,
+    rootElement
+  );
+} else {
+  render(
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>,
+    rootElement
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
